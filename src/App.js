cf39a8a3da70e5d83e46854de2338fd72ad2318a
Markus Risuaho <t1rima00@students.oamk.fi>
2022-10-16 17:57:56 +0300

    Weather toimii

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Weather from './weather';
+
+import {useState,useEffect} from 'react';
+
+
+
+
 
 function App() {
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [isLoading,setIsLoading] = useState (false);
+
+  useEffect(() =>  {
+    if(navigator.geolocation){
+      setIsLoading(true);
+      navigator.geolocation.getCurrentPosition(position => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+        setIsLoading(false);
+      },(error)=>{
+        alert(error);
+      })
+    }
+    else{
+      alert('Your browser does not support geolocating!')
+    }
+      }, [])
+    
+  
+
+ if (isLoading) {
+  return <p class='Lataus'> Loading, please wait... If the page isn't loading, grant access to your location </p>
+ }
+ else {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <h3>Your position</h3>
+      <p>
+        Position:&nbsp;
+        {lat.toFixed(3)},
+        {lng.toFixed(3)}
+      </p>
+      <Weather lat = {lat} lng={lng}/>
     </div>
   );
+ }
 }
 
 export default App;
---
+++ src/weather.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import axios from 'axios';
+import { useState,useEffect } from 'react';
+
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+const ICON_URL = 'http://openweathermap.org/img/wn/';
+const API_KEY = 'a31fcc4673020c9aaf1f6af3b2eaf8c2';
+
+export default function Weather({lat,lng}) {
+    const [temp, setTemp] = useState(0);
+    const [speed, setSpeed] = useState(0);
+    const [direction, setDirection] = useState(0);
+    const [description, setDescription] = useState('');
+    const [icon, setIcon] = useState('');
+
+useEffect(() => {
+    const address = API_URL +
+    '&lat=' + lat +
+    '&lon=' + lng +
+    '&units=metric' +
+    '&appid=' + API_KEY;
+
+    console.log(address);
+
+    axios.get(address).then((response) => {
+      console.log(response.data);
+      setTemp(response.data.main.temp);
+      setSpeed(response.data.wind.speed);
+      setDirection(response.data.wind.deg);
+      setDescription(response.data.weather[0].description);
+      setIcon(ICON_URL + response.data.weather[0].icon + '@2x.png');
+      console.log(ICON_URL + response.data.weather[0].icon + '@2x.png');
+    }).catch (error => { 
+      alert(error);
+    });
+    },[])
+  
+  return (
+<>
+<h3>The weather in your location</h3>
+<p>{temp} C&#176; </p>
+<p>{speed} m/s {direction} </p>
+<p class='desc'>{description}</p>
+<img src={icon} alt=""/>
+</>
+  );
+}
